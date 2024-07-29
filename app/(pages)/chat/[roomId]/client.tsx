068e382db7dafebb10650fbfa45c41b21ffe6ba2
7FyD <80410162+7FyD@users.noreply.github.com>
2024-07-29 13:41:53 +0300

    feat: add keydown event listener for sending messages

--- app/(pages)/chat/[roomId]/client.tsx
+++ app/(pages)/chat/[roomId]/client.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/navigation";
 import io, { Socket } from "socket.io-client";
 import { Input } from "@/app/components/ui/input";
 import { Button } from "@/app/components/ui/button";
@@ -18,28 +17,26 @@ const ChatClient: React.FC<{ roomId: string }> = ({ roomId }) => {
   const [input, setInput] = useState("");
   const [user, setUser] = useState("Anonymous user");
   const [messages, setMessages] = useState<Message[]>([]);
-  const router = useRouter();
   const messagesEndRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    if (roomId) {
-      socket = io();
-      socket.emit("join room", roomId);
+    socket = io();
+    socket.emit("join room", roomId);
 
-      socket.on("chat history", (history: Message[]) => {
-        setMessages(history);
-      });
+    socket.on("chat history", (history: Message[]) => {
+      setMessages(history);
+    });
 
-      socket.on("message", (message: Message) => {
-        setMessages((prevMessages) => [...prevMessages, message]);
-      });
+    socket.on("message", (message: Message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
 
-      return () => {
-        socket.off("chat history");
-        socket.off("message");
-        socket.disconnect();
-      };
-    }
-  }, [roomId]);
+    return () => {
+      socket.off("chat history");
+      socket.off("message");
+      socket.disconnect();
+    };
+  }, []);
 
   useEffect(() => {
     scrollToBottom();
@@ -60,54 +57,54 @@ const ChatClient: React.FC<{ roomId: string }> = ({ roomId }) => {
     }
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      sendMessage();
+    }
+  };
+
   return (
     <div className="container">
-      {!roomId && (
-        <h2 className="font-bold text-3xl text-center text-red-500">
-          Oops! Something went wrong.
-        </h2>
-      )}
-      {roomId && (
-        <div className="flex flex-col justify-start items-center gap-12">
-          <Input
-            value={user}
-            type="text"
-            maxLength={20}
-            onChange={(e) => setUser(e.target.value)}
-            placeholder="Enter username"
-            className="max-w-xs"
-          />
-          <div className="space-y-4">
-            <div
-              id="messagesContainer"
-              className="min-h-80 max-h-80 w-[56rem] break-words overflow-y-auto border-2 p-4 rounded-xl"
-              ref={messagesEndRef}
-            >
-              {messages.length === 0 && (
-                <p className="font-light">Chat is empty...</p>
-              )}
-              {messages.map((msg, index) => (
-                <div key={index}>
-                  <span>{new Date(msg.timestamp).toLocaleTimeString()} </span>
-                  <span>{msg.user}: </span>
-                  <span>{msg.text}</span>
-                </div>
-              ))}
-            </div>
-            <Input
-              value={input}
-              type="text"
-              maxLength={320}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="Type a message"
-              className="max-w-lg"
-            />
-            <Button variant={"outline"} onClick={sendMessage}>
-              Send
-            </Button>
+      <div className="flex flex-col justify-start items-center gap-12">
+        <Input
+          value={user}
+          type="text"
+          maxLength={20}
+          onChange={(e) => setUser(e.target.value)}
+          placeholder="Enter username"
+          className="max-w-xs"
+        />
+        <div className="space-y-4">
+          <div
+            id="messagesContainer"
+            className="min-h-80 max-h-80 w-[56rem] break-words overflow-y-auto border-2 p-4 rounded-xl"
+            ref={messagesEndRef}
+          >
+            {messages.length === 0 && (
+              <p className="font-light">Chat is empty...</p>
+            )}
+            {messages.map((msg, index) => (
+              <div key={index}>
+                <span>{new Date(msg.timestamp).toLocaleTimeString()} </span>
+                <span>{msg.user}: </span>
+                <span>{msg.text}</span>
+              </div>
+            ))}
           </div>
+          <Input
+            value={input}
+            type="text"
+            maxLength={320}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            className="max-w-lg"
+            onKeyDown={handleKeyDown}
+          />
+          <Button variant={"outline"} onClick={sendMessage}>
+            Send
+          </Button>
         </div>
-      )}
+      </div>
     </div>
   );
 };

ba0ab667b52ce4810b279e28a08ce6cb2af6ee4d
7FyD <80410162+7FyD@users.noreply.github.com>
2024-07-31 00:21:04 +0300

    feat: add basic header

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <Link href="/" className="text-center">
+      Go home
+    </Link>
+  );
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`min-h-screen flex flex-col ${inter.className}`}>
-        <header className="text-center">header</header>
+        <header className="text-center">
+          <Header />
+        </header>
         <main className="mb-auto">{children}</main>
         <footer className="text-center">footer</footer>
       </body>

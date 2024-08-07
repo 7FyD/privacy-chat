edc89145dc7b470690e856a6297c1af71cab74a7
7FyD <alexvlad1313@gmail.com>
2024-08-07 04:10:35 +0300

    style: change home page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,38 +1,7 @@
-import Link from "next/link";
+import Home from "../components/Home";
 
 const Page = () => {
-  return (
-    <div className="container mt-24 space-y-10">
-      <div className="space-y-2">
-        <h2 className="text-center text-4xl">
-          Looking to have a private, quick conversation?
-        </h2>
-        <h4 className="text-center text-4xl font-light">We've got you!</h4>
-      </div>
-      <Link
-        href="/chat"
-        className="mx-auto block bg-red-500 hover:bg-red-700 transition-colors rounded-full p-4 text-white max-w-max"
-      >
-        Start a one-to-one conversation!
-      </Link>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        temporibus cumque voluptatibus deserunt minus natus. Adipisci deleniti
-        aliquam perspiciatis magni eum, hic nobis laboriosam recusandae earum
-        necessitatibus animi reprehenderit neque.
-      </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 text-center gap-24 !mt-24">
-        <p>Image</p>
-        <p>Image</p>
-        <p>Image</p>
-        <p>Image</p>
-        <p>Image</p>
-        <p>Image</p>
-        <p>Image</p>
-        <p>Image</p>
-      </div>
-    </div>
-  );
+  return <Home />;
 };
 
 export default Page;
---
+++ app/components/Home/index.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+
+const Home = () => {
+  return (
+    <div className="container flex flex-col items-center mt-24 space-y-10">
+      <div className="space-y-4">
+        <h2 className="text-center text-2xl md:text-4xl">
+          Looking to have a private, quick conversation?
+        </h2>
+        <h4 className="text-center text-2xl md:text-4xl font-light">
+          We've got you!
+        </h4>
+      </div>
+      <Button asChild className="p-6 md:text-lg">
+        <Link href="/chat">Click to create your own private chat!</Link>
+      </Button>
+      <Separator className="max-w-5xl" />
+      <p className="md:text-xl font-light !mt-4 max-w-4xl text-center">
+        Create a secure and private chat, share it with friends or business
+        partner via a link, and enjoy safe conversations in a temporary, fully
+        privacy-based environment.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 text-center gap-24 !mt-24">
+        <p>Image</p>
+        <p>Image</p>
+        <p>Image</p>
+        <p>Image</p>
+      </div>
+      <Separator className="max-w-5xl" />
+    </div>
+  );
+};
+
+export default Home;
